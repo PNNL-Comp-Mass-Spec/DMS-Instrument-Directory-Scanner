b8cff55c3e4e03485e4aa2457ad160ec45aa1708
alchemistmatt <matthew.monroe@pnnl.gov>
2017-10-09 13:08:40 -0700

    Update to .NET 4.6.2 and retrieve libraries from NuGet

--- DMS_InstDirScanner/My Project/AssemblyInfo.vb
+++ DMS_InstDirScanner/My Project/AssemblyInfo.vb
@@ -1,35 +1,33 @@
-﻿Imports System
-Imports System.Reflection
+﻿Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-' General Information about an assembly is controlled through the following 
+' General Information about an assembly is controlled through the following
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("DMS_InstDirScanner")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("PNNL")> 
-<Assembly: AssemblyProduct("DMS_InstDirScanner")> 
-<Assembly: AssemblyCopyright("Copyright © PNNL 2009")> 
-<Assembly: AssemblyTrademark("")> 
+<Assembly: AssemblyTitle("DMS_InstDirScanner")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("PNNL")>
+<Assembly: AssemblyProduct("DMS_InstDirScanner")>
+<Assembly: AssemblyCopyright("Copyright © PNNL 2009")>
+<Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("90c8c928-d237-4b58-a2c0-7c38715b5bb2")> 
+<Assembly: Guid("90c8c928-d237-4b58-a2c0-7c38715b5bb2")>
 
 ' Version information for an assembly consists of the following four values:
 '
 '      Major Version
-'      Minor Version 
+'      Minor Version
 '      Build Number
 '      Revision
 '
-' You can specify all the values or you can default the Build and Revision Numbers 
+' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")> 
+' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.3.*")> 
-'<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("1.3.*")>

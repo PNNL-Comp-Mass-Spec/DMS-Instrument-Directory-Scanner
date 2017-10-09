84c44d7c5eac42fd06f54f456c4b78507d01a7bb
alchemistmatt <matthew.monroe@pnnl.gov>
2017-10-09 13:13:32 -0700

    Use auto properties

--- DMS_InstDirScanner/clsInstData.vb
+++ DMS_InstDirScanner/clsInstData.vb
@@ -1,61 +1,29 @@
 ﻿'*********************************************************************************************************
-' Written by Dave Clark for the US Department of Energy 
+' Written by Dave Clark for the US Department of Energy
 ' Pacific Northwest National Laboratory, Richland, WA
 ' Copyright 2009, Battelle Memorial Institute
 ' Created 07/27/2009
 '
-' Last modified 07/27/2009
 '*********************************************************************************************************
 
+''' <summary>
+''' Class to hold data for each instrument
+''' </summary>
 Public Class clsInstData
 
-	'*********************************************************************************************************
-	' Class to hold data for each instrument
-	'*********************************************************************************************************
-
 #Region "Module variables"
-	Private m_StorageVol As String
-	Private m_StoragePath As String
-	Private m_CapMethod As String
-	Private m_InstName As String
+
 #End Region
 
 #Region "Properties"
-	Public Property StorageVolume() As String
-		Get
-			Return m_StorageVol
-		End Get
-		Set(ByVal value As String)
-			m_StorageVol = value
-		End Set
-	End Property
 
-	Public Property StoragePath() As String
-		Get
-			Return m_StoragePath
-		End Get
-		Set(ByVal value As String)
-			m_StoragePath = value
-		End Set
-	End Property
+    Public Property StorageVolume As String
 
-	Public Property CaptureMethod() As String
-		Get
-			Return m_CapMethod
-		End Get
-		Set(ByVal value As String)
-			m_CapMethod = value
-		End Set
-	End Property
+    Public Property StoragePath As String
 
-	Public Property InstName() As String
-		Get
-			Return m_InstName
-		End Get
-		Set(ByVal value As String)
-			m_InstName = value
-		End Set
-	End Property
+    Public Property CaptureMethod As String
+
+    Public Property InstName As String
+
 #End Region
-
 End Class

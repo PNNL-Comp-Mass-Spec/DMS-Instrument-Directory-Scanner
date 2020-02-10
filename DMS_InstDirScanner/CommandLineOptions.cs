c3398edbd8b8db8e8c4c4077fb339557d3dfcd74
Bryson Gibbons <bryson.gibbons@pnnl.gov>
2020-02-10 14:50:52 -0800

    Update to use CommandLineParser instead of clsParseCommandLine

---
+++ DMS_InstDirScanner/CommandLineOptions.cs
@@ -0,0 +1,13 @@
+﻿using PRISM;
+
+namespace DMS_InstDirScanner
+{
+    internal class CommandLineOptions
+    {
+        [Option("noBionet", HelpShowsDefault = false, HelpText = "Skip instruments on bionet")]
+        public bool NoBionet { get; set; }
+
+        [Option("preview", HelpShowsDefault = false, HelpText = "Search for files and directories, but do not update any files on the DMS_InstSourceDirScans share")]
+        public bool PreviewMode { get; set; }
+    }
+}
--- DMS_InstDirScanner/Program.cs
+++ DMS_InstDirScanner/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using PRISM;
 using PRISM.FileProcessor;
 using PRISM.Logging;
@@ -12,11 +13,7 @@ namespace DMS_InstDirScanner
     /// </summary>
     internal class Program
     {
-        public const string PROGRAM_DATE = "February 7, 2020";
-
-        private static bool mNoBionet;
-
-        private static bool mPreviewMode;
+        public const string PROGRAM_DATE = "February 10, 2020";
 
         /// <summary>
         /// Entry method
@@ -24,50 +21,39 @@ namespace DMS_InstDirScanner
         /// <returns>0 if no error, error code if an error</returns>
         public static int Main(string[] args)
         {
-            var commandLineParser = new clsParseCommandLine();
-
-            mNoBionet = false;
-
-            mPreviewMode = false;
-
             try
             {
-                bool validArgs;
+                var exeName = System.IO.Path.GetFileName(System.Reflection.Assembly.GetExecutingAssembly().GetName().Name);
 
-                // Parse the command line options
-                if (commandLineParser.ParseCommandLine())
+                var cmdLineParser = new CommandLineParser<CommandLineOptions>(exeName,
+                    ProcessFilesOrDirectoriesBase.GetAppVersion(PROGRAM_DATE))
                 {
-                    validArgs = SetOptionsUsingCommandLineParameters(commandLineParser);
-                }
-                else if (commandLineParser.NoParameters)
-                {
-                    validArgs = true;
-                }
-                else
-                {
-                    if (commandLineParser.NeedToShowHelp)
-                    {
-                        ShowProgramHelp();
-                    }
-                    else
-                    {
-                        ConsoleMsgUtils.ShowWarning("Error parsing the command line arguments");
-                        clsParseCommandLine.PauseAtConsole(750);
-                    }
+                    ProgramInfo = "This program finds the files and directories in the source folder for active DMS instruments. " +
+                                  "It creates a text file for each instrument on a central share, listing the files and directories." +
+                                  "On the DMS website, the helper_inst_source page file reads these text files show DMS users dataset" +
+                                  "files and directories on the instruments, for example https://dms2.pnl.gov/helper_inst_source/view/QExactP02",
+                    ContactInfo =
+                        "Program written by Dave Clark and Matthew Monroe for the Department of Energy (PNNL, Richland, WA)" +
+                        Environment.NewLine +
+                        "E-mail: matthew.monroe@pnnl.gov or proteomics@pnnl.gov" + Environment.NewLine +
+                        "Website: https://omics.pnl.gov/ or https://panomics.pnnl.gov/" + Environment.NewLine + Environment.NewLine +
+                        "Licensed under the 2-Clause BSD License; you may not use this file except in compliance with the License. " +
+                        "You may obtain a copy of the License at https://opensource.org/licenses/BSD-2-Clause"
+                };
 
-                    return -1;
-                }
-
-                if (commandLineParser.NeedToShowHelp || !validArgs)
+                var parsed = cmdLineParser.ParseArgs(args, false);
+                var options = parsed.ParsedResults;
+                if (!parsed.Success)
                 {
-                    ShowProgramHelp();
+                    // Delay for 1500 msec in case the user double clicked this file from within Windows Explorer (or started the program via a shortcut)
+                    Thread.Sleep(1500);
                     return -1;
                 }
 
                 var mainProcess = new MainProcess
                 {
-                    NoBionet = mNoBionet,
-                    PreviewMode = mPreviewMode
+                    NoBionet = options.NoBionet,
+                    PreviewMode = options.PreviewMode
                 };
 
                 try
@@ -99,103 +85,11 @@ namespace DMS_InstDirScanner
                 LogTools.FlushPendingMessages();
                 return -1;
             }
-
-        }
-
-        private static bool SetOptionsUsingCommandLineParameters(clsParseCommandLine commandLineParser)
-        {
-            // Returns True if no problems; otherwise, returns false
-            var validParameters = new List<string>
-            {
-                "NoBionet",
-                "Preview"
-            };
-
-            try
-            {
-                // Make sure no invalid parameters are present
-                if (commandLineParser.InvalidParametersPresent(validParameters))
-                {
-                    ShowErrorMessage("Invalid command line parameters",
-                        (from item in commandLineParser.InvalidParameters(validParameters) select "/" + item).ToList());
-
-                    return false;
-                }
-
-                // Query commandLineParser to see if various parameters are present
-                if (commandLineParser.IsParameterPresent("NoBionet"))
-                {
-                    mNoBionet = true;
-                }
-
-                if (commandLineParser.IsParameterPresent("Preview"))
-                {
-                    mPreviewMode = true;
-                }
-
-                return true;
-
-            }
-            catch (Exception ex)
-            {
-                ShowErrorMessage("Error parsing the command line parameters: ", ex);
-            }
-
-            return false;
         }
 
         private static void ShowErrorMessage(string message, Exception ex)
         {
             ConsoleMsgUtils.ShowError(message, ex);
         }
-
-        private static void ShowErrorMessage(string title, IEnumerable<string> errorMessages)
-        {
-            ConsoleMsgUtils.ShowErrors(title, errorMessages);
-        }
-
-        private static void ShowProgramHelp()
-        {
-            try
-            {
-                var exePath = MainProcess.GetAppPath();
-
-                Console.WriteLine(ConsoleMsgUtils.WrapParagraph(
-                    "This program finds the files and directories in the source folder for active DMS instruments. " +
-                    "It creates a text file for each instrument on a central share, listing the files and directories." +
-                    "On the DMS website, the helper_inst_source page file reads these text files show DMS users dataset" +
-                    "files and directories on the instruments, for example https://dms2.pnl.gov/helper_inst_source/view/QExactP02"));
-                Console.WriteLine();
-                Console.WriteLine("Program syntax:");
-                Console.WriteLine(exePath + " [/NoBionet] [/Preview]");
-                Console.WriteLine();
-                Console.WriteLine("Use /NoBionet to skip instruments on bionet");
-                Console.WriteLine();
-                Console.WriteLine(ConsoleMsgUtils.WrapParagraph(
-                    "Use /Preview to search for files and directories, but not update any files on the DMS_InstSourceDirScans share"));
-                Console.WriteLine();
-                Console.WriteLine(ConsoleMsgUtils.WrapParagraph(
-                    "Program written by Dave Clark and Matthew Monroe for the Department of Energy (PNNL, Richland, WA)"));
-                Console.WriteLine();
-                Console.WriteLine("Version: " + ProcessFilesOrDirectoriesBase.GetAppVersion(PROGRAM_DATE));
-                Console.WriteLine();
-                Console.WriteLine("E-mail: matthew.monroe@pnnl.gov or proteomics@pnnl.gov");
-                Console.WriteLine("Website: https://omics.pnl.gov/ or https://panomics.pnnl.gov/");
-                Console.WriteLine();
-                Console.WriteLine(ConsoleMsgUtils.WrapParagraph(
-                    "Licensed under the 2-Clause BSD License; you may not use this file except in compliance with the License. " +
-                    "You may obtain a copy of the License at https://opensource.org/licenses/BSD-2-Clause"));
-                Console.WriteLine();
-
-                // Delay for 750 msec in case the user double clicked this file from within Windows Explorer (or started the program via a shortcut)
-                clsParseCommandLine.PauseAtConsole(750);
-            }
-            catch (Exception ex)
-            {
-                ShowErrorMessage("Error displaying the program syntax", ex);
-            }
-
-        }
-
     }
 }

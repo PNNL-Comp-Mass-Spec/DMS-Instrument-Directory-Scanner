01134d63691c62f0d06469dbb6a9486f26cdd836
alchemistmatt <matthew.monroe@pnnl.gov>
2022-04-18 13:11:50 -0700

    Remove blank line and unused using statement

--- DMS_InstDirScanner/Properties/AssemblyInfo.cs
+++ DMS_InstDirScanner/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -31,4 +30,3 @@ using System.Runtime.InteropServices;
 //
 
 [assembly: AssemblyVersion("1.4.*")]
-

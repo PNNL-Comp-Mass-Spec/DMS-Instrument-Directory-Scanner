80f521026c510e4ccb96626df4db86e159becadc
alchemistmatt <matthew.monroe@pnnl.gov>
2023-01-21 15:27:08 -0800

    Suppress warnings and add dictionary word

---
+++ DMS_InstDirScanner/GlobalSuppressions.cs
@@ -0,0 +1,9 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Roslynator", "RCS1075:Avoid empty catch clause that catches System.Exception.", Justification = "Safe to ignore", Scope = "member", Target = "~M:DMS_InstDirScanner.MessageSender.DestroyConnection")]
+[assembly: SuppressMessage("Roslynator", "RCS1075:Avoid empty catch clause that catches System.Exception.", Justification = "Safe to ignore", Scope = "member", Target = "~M:DMS_InstDirScanner.MessageSender.Dispose")]
--- DMS_InstDirScanner/StatusFile.cs
+++ DMS_InstDirScanner/StatusFile.cs
@@ -522,6 +522,7 @@ namespace DMS_InstDirScanner
                 m_MessageQueueExceptionCount++;
                 var msg = "Exception sending status message to broker; count = " + m_MessageQueueExceptionCount;
 
+                // ReSharper disable once MergeIntoPattern
                 if (DateTime.Now.TimeOfDay.Hours == 0 && DateTime.Now.TimeOfDay.Minutes is >= 0 and <= 5)
                 {
                     // The time of day is between 12:00 am and 12:10 am, so write the full exception to the log

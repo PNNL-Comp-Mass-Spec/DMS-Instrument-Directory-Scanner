6ab4f799e390e644fcf44eda4e665c94e162798b
alchemistmatt <matthew.monroe@pnnl.gov>
2024-06-20 19:50:25 -0700

    Remove deprecated URL

--- DMS_InstDirScanner/Program.cs
+++ DMS_InstDirScanner/Program.cs
@@ -38,7 +38,7 @@ namespace DMS_InstDirScanner
                     ContactInfo =
                         "Program written by Dave Clark and Matthew Monroe for the Department of Energy (PNNL, Richland, WA)" + Environment.NewLine +
                         "E-mail: matthew.monroe@pnnl.gov or proteomics@pnnl.gov" + Environment.NewLine +
-                        "Website: https://github.com/PNNL-Comp-Mass-Spec/ or https://panomics.pnnl.gov/ or https://www.pnnl.gov/integrative-omics" + Environment.NewLine + Environment.NewLine +
+                        "Website: https://github.com/PNNL-Comp-Mass-Spec/ or https://www.pnnl.gov/integrative-omics" + Environment.NewLine + Environment.NewLine +
                         "Licensed under the 2-Clause BSD License; you may not use this file except in compliance with the License. " +
                         "You may obtain a copy of the License at https://opensource.org/licenses/BSD-2-Clause"
                 };
